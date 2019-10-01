f73951aa4ee02a00b20455d7debdb8191a66c997
Ahmad Nazir Raja <ahmadnazir@gmail.com>
2019-10-01 15:12:23 +0200

    Fixed the version

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject cli/lein-template "0.1.0-SNAPSHOT"
-  :description "Create clojure apps that can be executed from the cli: `lein run`"
+(defproject cli/lein-template "0.1.0"
+  :description "Leinigen template for creating native cli apps"
   :url "http://github.com/ahmadnazir/cli-lein-template"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
