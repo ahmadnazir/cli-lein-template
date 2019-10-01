b852523b26c4c3e5cf8cfdb7f180b25490b7844a
Ahmad Nazir Raja <ahmadnazir@gmail.com>
2019-10-01 16:25:05 +0200

    Bumped the version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cli/lein-template "0.1.2"
+(defproject cli/lein-template "0.1.3"
   :description "Leinigen template for creating native cli apps"
   :url "http://github.com/ahmadnazir/cli-lein-template"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
