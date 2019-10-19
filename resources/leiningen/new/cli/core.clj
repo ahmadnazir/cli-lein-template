317e3ec6d9e9b3503e3dc894f1c05082030930c9
Ahmad Nazir Raja <ahmadnazir@gmail.com>
2019-10-19 22:11:29 +0200

    Handling input from stdin

--- resources/leiningen/new/cli/core.clj
+++ resources/leiningen/new/cli/core.clj
@@ -1,5 +1,16 @@
 (ns {{name}}.core
     (:gen-class))
 
+(defn handle [line]
+  (println (str " :: " line)))
+
 (defn -main [& args]
-  (println "Hello world!"))
+  (loop [
+         lines (line-seq (java.io.BufferedReader. *in*))
+         ]
+    (let [line (first lines)]
+      (cond line (do
+                   (handle line)
+                   (recur (rest lines)))
+            :else (println "Done!"))
+      )))
